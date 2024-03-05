39ea4e290ab5ef28121365fc2260c7e67e7f6f91
Orlando Jimenez <orlandojn10@gmail.com>
2024-03-05 22:42:19 +0100

    chore: installing routing dependency and setting views routes

--- marvel-btw-test/src/App.tsx
+++ marvel-btw-test/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Link, Route, Switch } from 'wouter';
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -7,28 +9,43 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Switch>
+      <Route
+        path='/'
+        component={() => (
+          <>
+            <div>
+              <Link href='/character/wolverine'>Profile</Link>
+              <a href='https://vitejs.dev' target='_blank'>
+                <img src={viteLogo} className='logo' alt='Vite logo' />
+              </a>
+              <a href='https://react.dev' target='_blank'>
+                <img src={reactLogo} className='logo react' alt='React logo' />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className='card'>
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className='read-the-docs'>
+              Click on the Vite and React logos to learn more
+            </p>
+          </>
+        )}
+      />
+
+      <Route path='/character/:name'>
+        {(params) => <>Hello, {params.name}!</>}
+      </Route>
+
+      {/* Default route in a switch */}
+      <Route>404: No such page!</Route>
+    </Switch>
   );
 }
 
