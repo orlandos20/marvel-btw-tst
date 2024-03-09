b3f68390a7c008b3aa273074f8c0fba95119ebbd
Orlando Jimenez <orlandojn10@gmail.com>
2024-03-09 02:22:57 +0100

    feat: search input component html && css

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,17 +1,35 @@
 import { MainLayout } from '@/views/layouts';
 import { CharacterCard } from '@/components/cards';
 import Grid from '@/components/grid/Grid';
+import SearchInput from '@/components/search-input/SearchInput';
 
 import { useGetAllCharactersUseCase } from '@/hooks/characters/useGetAllCharactersUseCase';
 import { useGetCharacterUseCase } from '@/hooks/characters/useGetCharacterUseCase';
 import { Character } from '@/modules/characters/domain/Character';
+import { FormEvent } from 'react';
 
 const Home = () => {
   const { characters, limit } = useGetAllCharactersUseCase();
   const { retrieveCharacter } = useGetCharacterUseCase();
 
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formValues = new FormData(e.target as HTMLFormElement);
+    const formValuesAsParams = new URLSearchParams(
+      formValues as unknown as Record<string, string>
+    ).toString();
+    console.log('formValues --> ', formValuesAsParams);
+  };
+
   return (
     <MainLayout>
+      <form onSubmit={submitHandler}>
+        <SearchInput
+          name="nameStartsWith"
+          placeholder="Search a Character..."
+          searchResultsLegend={`${characters?.length} results`}
+        />
+      </form>
       <Grid>
         {characters?.length > 0 &&
           characters?.map((character: Character) => (
