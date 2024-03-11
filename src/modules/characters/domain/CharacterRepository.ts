8f6990fbd5c4ef0f0877c12ba000aec3740cabc9
Orlando Jimenez <orlandojn10@gmail.com>
2024-03-11 20:47:50 +0100

    fix: import a type from shared types folder

--- src/modules/characters/domain/CharacterRepository.ts
+++ src/modules/characters/domain/CharacterRepository.ts
@@ -1,20 +1,7 @@
+import { DefaultParams, Hasher } from '@/src/types';
 import { Character } from './Character';
 import { MarvelResponseWrapper } from '@/types/marvelApiResponseTypes';
 
-export interface DefaultParams {
-  limit: string | number;
-  offset: string | number;
-  orderBy?: 'name' | 'modified' | '-name' | '-modified';
-  name?: string;
-  nameStartsWith?: string | undefined;
-  modifiedSince?: Date | undefined;
-  comics?: number | undefined;
-  series?: number | undefined;
-  events?: number | undefined;
-  stories?: number | undefined;
-}
-
-export type Hasher = (toHash: string) => string;
 export type GetParams = {
   hasher: Hasher;
   params?: DefaultParams;
